736759803b94e98b554485fa807ac26327da6519
Agbooa12 <agboolaelijah077@gmail.com>
2023-10-21 23:20:21 +0100

    working on the user register and login page

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -22,52 +22,54 @@ const createUser = async (req, res) => {
     User.create(usr)
         .then((_result) => {
             res.json({
-                "message": "created successfully",
+                message: "created successfully",
                 status: true
             })
         }).catch((_error) => {
             res.json({
-                "message": "failed to created user",
+                message: "failed to created user",
                 status: false
             })
         })
 
 }
 
-const loginUser = async (req, res, next) => {
-    const { dataValues } = await User.findOne({ where: { email: req.body.email } })
-    if (dataValues) {
-        // console.log(dataValues)
-        const password_valid = await bcrypt.compare(req.body.password, dataValues.passWord);
-        if (password_valid) {
-            token = await jwt.sign({ "id": dataValues.id, "email": dataValues.email, "firstName": dataValues.firstName }, process.env.SECRET);
-            res.status(200).json({ token: token });
+const loginUser = async(req, res)=>{
+    const {email, passWord} = req.body;
+    try{
+        const user = await User.findOne({
+            where: {email}
+        })
+        if(!user){
+            return res.status({
+                status: false,
+                message: "email not correct"
+            }) 
         }
-        else {
-            res.status(400).json({ error: "Password Incorrect" });
-        }
-
-    } else {
-        res.status(404).json({ error: "User does not exist" });
+        const passwordMatch = await bcrypt.compare(passWord, user.passWord);
+  
+      if (passwordMatch) {
+        const token = jwt.sign({ email: user.email, id: user.id }, process.env.JWT_SECRET, {
+          expiresIn: "12h", 
+        });
+  
+        return res.status(200).json({ 
+            message: "Login successful", 
+            status: true, 
+            token 
+        });
+      } 
+      else {
+        return res.status(401).json({ 
+            message: "Wrong information ", 
+            status: false 
+        });
+      }
     }
-};
+    catch{
 
-
-// app.post('/login', async (req, res) => {
-//     const { username, password } = req.body;
-  
-//     try {
-//       const user = await User.findOne({ where: { username } });
-  
-//       if (!user || user.password !== password) {
-//         res.status(401).json({ message: 'Invalid credentials' });
-//       } else {
-//         res.json({ message: 'Login successful', user });
-//       }
-//     } catch (error) {
-//       res.status(500).json({ message: 'Error logging in', error });
-//     }
-//   });
+    }
+}
 
 
 module.exports = { createUser, loginUser }
