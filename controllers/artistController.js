c18871be17d5054e6547d6281080c1ee31a87ee0
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-04 22:23:18 +0100

    adding some backend code to the new add feature

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -179,7 +179,7 @@ const getDjs = async (req, res)=> {
  
   try {
       const user = await Artist.findAll({where:{djs}});
-      // console.log(user);
+      console.log(user);
       if (!user) {
         return res.status(404).json({ status: false, error: "Artist Band not found" });
       }
