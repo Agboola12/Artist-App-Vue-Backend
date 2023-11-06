7b6fa3e45de5fc96b5090a3f69019c99f517c733
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-06 01:12:31 +0100

    working on the new feature

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -162,6 +162,19 @@ const getAllArtist = async(req,res)=>{
 
 const getBand = async (req, res)=> {
  
+  try {
+    const user = await Artist.findAll({ where: { musicType: 'Bands' } });
+    console.log(user);
+  
+    if (!user || user.length === 0) {
+      return res.status(404).json({ status: false, error: "Artist Band not found" });
+    }
+  
+    res.status(200).json({ status: true, data: user });
+  } catch (error) {
+    console.error("Error fetching artist band details:", error);
+    res.status(500).json({ status: false, error: "Error fetching artist band details" });
+  }
   
 }
 
