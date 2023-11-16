1e4b44d2da3d0e93b631c13887c699ffbff6d8eb
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-16 14:08:47 +0100

    working on backend

--- controllers/bookingInfoController.js
+++ controllers/bookingInfoController.js
@@ -5,10 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const createArtist = async (req, res) => {
-    const imageUrl = (req.file.path);
     try {
-      
-      const salt = await bcrypt.genSalt(10);
       const info = {
         firstName: req.body.firstName,
         email: req.body.email,
@@ -16,10 +13,8 @@ const createArtist = async (req, res) => {
         mobile: req.body.mobile,
         state: req.body.state,
         country: req.body.country,
-        imageUrl: imageUrl,
-        passWord: await bcrypt.hash(req.body.passWord, salt)
       }
-      Artist.create(info)
+      BookingInfo.create(info)
         .then((response) => {
           res.json({
             message: "artist created successfully",
