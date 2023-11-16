00637c25562f98bd52b34ccc31da68da9dc979c3
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-16 14:05:24 +0100

    working on backend

--- controllers/bookingInfoController.js
+++ controllers/bookingInfoController.js
@@ -4,3 +4,41 @@ const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const createArtist = async (req, res) => {
+    
+      const salt = await bcrypt.genSalt(10);
+      const info = {
+        firstName: req.body.firstName,
+        email: req.body.email,
+        musicType: req.body.musicType,
+        mobile: req.body.mobile,
+        state: req.body.state,
+        country: req.body.country,
+        imageUrl: imageUrl,
+        passWord: await bcrypt.hash(req.body.passWord, salt)
+      }
+      Artist.create(info)
+        .then((response) => {
+          res.json({
+            message: "artist created successfully",
+            status: true
+          })
+        })
+        .catch((err) => {
+          res.status(400).json({
+            message: " error in artist creating ",
+            status: false
+          })
+          console.log(err);
+        })
+  
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({
+        message: "Internal server error",
+        status: false,
+        error: error.message,
+      })
+    }
+  
+  }
