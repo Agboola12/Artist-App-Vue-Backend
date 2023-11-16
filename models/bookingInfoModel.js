d24cdc74bff252ebc20924c4aa10948a7138193c
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-16 13:48:15 +0100

    working on backend

--- models/bookingInfoModel.js
+++ models/bookingInfoModel.js
@@ -25,6 +25,31 @@ class Artist extends Model{};
         type: DataTypes.STRING,
         allowNull: false
     },
+    date:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    event:{
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false
+    },
+    information:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    musicType:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    state:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    time:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
 
  },{
         sequelize,
