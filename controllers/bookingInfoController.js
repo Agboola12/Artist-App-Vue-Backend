dc4d6ce1f13b96d1983c7e43acf1fe9fee9aeebf
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-16 14:03:21 +0100

    working on backend

---
+++ controllers/bookingInfoController.js
@@ -0,0 +1,6 @@
+const BookingInfo = require("../models/bookingInfoModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
