f722d46b6b739a29d8d8076f1300fe01bedcdf74
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-28 15:48:32 +0100

    working on the node mailer

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -32,34 +32,34 @@ const createUser = async (req, res) => {
     //             status: false
     //         })
     //     })
-    // User.create({usr}, 
-    //     async (err, message) =>{
-    //     if (err){
-    //         res.status(400).json({
-    //             success: false,
-    //             message : "An error in creating an account"
-    //         })
-    //         console.log(err);
-    //     }else{
-    //     try {
-    //        await sendMail({
-    //         to: email,
-    //         subject: "succesful",
-    //         html:`
-    //             <div>
-    //                 <h3>welcome ${firstName} to prime website</h3>
-    //             </div>
-    //         `
-    //        })
-    //     } catch (error) {
-    //         log.console("An error occureed while sending mail")
-    //     }
-    //         res.json({
-    //             success: true,
-    //             message: "User registration successful",
-    //         })
-    //     }
-    // })
+    User.create({usr}, 
+        async (err, message) =>{
+        if (err){
+            res.status(400).json({
+                success: false,
+                message : "An error in creating an account"
+            })
+            console.log(err);
+        }else{
+        try {
+           await sendMail({
+            to: email,
+            subject: "succesful",
+            html:`
+                <div>
+                    <h3>welcome ${firstName} to prime website</h3>
+                </div>
+            `
+           })
+        } catch (error) {
+            log.console("An error occureed while sending mail")
+        }
+            res.json({
+                success: true,
+                message: "User registration successful",
+            })
+        }
+    })
 
 }
 
