6fc642aa7bdbeebbee07429a419e81f8694acb38
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-30 09:19:58 +0100

    working on the node mailer

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const { verifyToken } = require("../middlewares/authMiddleware");
 const { createMusic, delSong, songDetail, getAllSong, updateSong, AllSong, popularSong   } = require("../controllers/musicController");
 const { upload} = require("../upload.js");
 const { bookingInfo, getBookingInfo, infoDetail, Approve } = require("../controllers/bookingInfoController.js");
-const { sendMail } = require("../mail.js");
+// const { sendMail } = require("../mail.js");
 
 const Router=express.Router();
 
@@ -28,7 +28,7 @@ Router.get('/getMusicArtist', getMusicArtist )
 Router.get('/getArtistDetails/:id', getArtistDetails )
 Router.get('/artistNotice/:id',  artistNotice )
 Router.get('/artistAppointment/:id',  artistAppointment )
-Router.get('/mail',  sendMail )
+// Router.get('/mail',  sendMail )
 
 //artist music
 Router.post('/createMusic', upload.fields([{name: 'image'}, {name:'mp3'}]), createMusic)
