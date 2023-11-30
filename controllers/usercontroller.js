676f893386352f5ae737d679cc3c6e5df50da5f6
Agbooa12 <agboolaelijah077@gmail.com>
2023-11-30 09:46:29 +0100

    working on the node mailer

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -2,58 +2,58 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-const { sendMail } = require("../mail");
+// const { sendMail } = require("../mail");
 // const nodemailer = require('nodemailer');
 // const { sendMail } = require("../mail");
 
 dotenv.config();
 
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: process.env.APP_MAIL,
-//         pass: process.env.APP_PASSWORD
-//     }
-    // auth: {
-    //   user: 'agboolaelijah077@gmail.com',
-    //   pass: '08060360214',
-    // },
-//   });
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.APP_MAIL,
+        pass: process.env.APP_PASSWORD
+    }
+    auth: {
+      user: 'agboolaelijah077@gmail.com',
+      pass: '08060360214',
+    },
+  });
 
-// const createUser = async (req, res) => {
-//     const user = await User.findOne({ where: { email: req.body.email } })
-//     if (user) {
-//         return res.status(200).json({
-//             message: "email already exist",
-//             status: false
-//         })
-//     }
-//     const salt = await bcrypt.genSalt(10);
-//     var usr = {
-//         firstName: req.body.firstName,
-//         email: req.body.email,
-//         passWord: await bcrypt.hash(req.body.passWord, salt)
-//     };
-//     await User.create(usr)
-//             await transporter.sendMail({
-//             from: 'elijahiyanuoluwa12@gmail.com',
-//             to: req.body.email,
-//             subject: 'Form Submission Confirmation',
-//             text: `Thank you for submitting the form, ${req.body.firstName}!`,
-//          });
+const createUser = async (req, res) => {
+    const user = await User.findOne({ where: { email: req.body.email } })
+    if (user) {
+        return res.status(200).json({
+            message: "email already exist",
+            status: false
+        })
+    }
+    const salt = await bcrypt.genSalt(10);
+    var usr = {
+        firstName: req.body.firstName,
+        email: req.body.email,
+        passWord: await bcrypt.hash(req.body.passWord, salt)
+    };
+    await User.create(usr)
+            await transporter.sendMail({
+            from: 'elijahiyanuoluwa12@gmail.com',
+            to: req.body.email,
+            subject: 'Form Submission Confirmation',
+            text: `Thank you for submitting the form, ${req.body.firstName}!`,
+         });
         
-//             res.json({
-//                 message: "User account created successfully",
-//                 status: true
-//             })        
-//         .catch((error) => {
-//             res.json({
-//                 message: "Failed to created user",
-//                 status: false
-//             })
-//             console.log(error);
-//         })
-// }
+            res.json({
+                message: "User account created successfully",
+                status: true
+            })        
+        .catch((error) => {
+            res.json({
+                message: "Failed to created user",
+                status: false
+            })
+            console.log(error);
+        })
+}
 
 // const createUser = async (req, res) => {
 //     try {
