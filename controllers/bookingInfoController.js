9a3992521df1a02d31baa24d0c7a18f089189583
Agbooa12 <agboolaelijah077@gmail.com>
2023-12-05 10:27:14 +0100

    working on the notification

--- controllers/bookingInfoController.js
+++ controllers/bookingInfoController.js
@@ -35,7 +35,7 @@ const bookingInfo = async (req, res) => {
         })
         .catch((err) => {
           res.status(200).json({
-            message: " error in Booking information creating ",
+            message: " You have to fill the booking form before you can hire Booking information creating ",
             status: false
           })
           console.log(err);
